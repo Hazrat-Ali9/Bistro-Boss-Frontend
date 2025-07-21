a49cf45e62de08e405329e77b70123563966ce6c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-21 09:01:08 +0600

    user auth  🚀

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
-
+// user auth
 const useAuth = () => {
     const auth = useContext(AuthContext);
     return auth
